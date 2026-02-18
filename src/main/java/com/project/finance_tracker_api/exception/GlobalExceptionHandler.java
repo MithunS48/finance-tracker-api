a2856997fb8de4bab0302a3abca006cda8e72edc
mithunsgowda48 <mithunsgowda18@gmail.com>
2026-02-18 22:13:27 +0530

    added some exception

--- src/main/java/com/project/finance_tracker_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/finance_tracker_api/exception/GlobalExceptionHandler.java
@@ -2,14 +2,35 @@ package com.project.finance_tracker_api.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleException(UserNotFoundException ex){
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> genericException(Exception ex){
+        return new ResponseEntity<>("Something went wrong",HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handlingValidation(MethodArgumentNotValidException ex)
+    {
+        Map<String,String> map=new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error->map.put(error.getField(),error.getDefaultMessage()));
+
+        return new ResponseEntity<>(map,HttpStatus.BAD_REQUEST);
+    }
+
 
 }
